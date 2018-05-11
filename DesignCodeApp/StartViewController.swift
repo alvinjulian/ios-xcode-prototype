8ec97ff4f903a2e4e640dd8a44ac8aebbf220022
omrobbie <putu.roby@yahoo.com>
2018-05-11 15:25:23 +0800

    header layout di storyboard

---
+++ DesignCodeApp/StartViewController.swift
@@ -0,0 +1,20 @@
+//
+//  StartViewController.swift
+//  DesignCodeApp
+//
+//  Created by omrobbie on 11/05/18.
+//  Copyright © 2018 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
