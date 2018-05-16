0344aeb66625be241b11456bcb3c5fb5d9a7fc62
omrobbie <putu.roby@yahoo.com>
2018-05-17 03:50:35 +0800

    gunakan static data untuk collection view

---
+++ DesignCodeApp/SectionCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SectionCollectionViewCell.swift
+//  DesignCodeApp
+//
+//  Created by omrobbie on 17/05/18.
+//  Copyright © 2018 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class SectionCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var coverImageView: UIImageView!
+    
+}
