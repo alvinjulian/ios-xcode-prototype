a55f0a3df9c5838ca34373589061217a18dd67db
omrobbie <putu.roby@yahoo.com>
2018-05-17 05:29:27 +0800

    perbaiki animasi collection view cell pada saat tampil pertama kali

--- DesignCodeApp/StartViewController.swift
+++ DesignCodeApp/StartViewController.swift
@@ -74,30 +74,34 @@ extension StartViewController: UIScrollViewDelegate {
                 
                 let translationX = cellFrame.origin.x / 8
                 cell.coverImageView.transform = CGAffineTransform(translationX: translationX, y: 0)
-
-                let angleFromX = Double(-cellFrame.origin.x / 10)
-                let angle = CGFloat((angleFromX * Double.pi) / 180)
-                var transform = CATransform3DIdentity
-                transform.m34 = -1.0 / 1000
-                let rotation = CATransform3DRotate(transform, angle, 0, 1, 0)
                 
-                var scaleFromX = (1000 - (cellFrame.origin.x - 200)) / 1000
-                let scaleMax: CGFloat = 1.0
-                let scaleMin: CGFloat = 0.6
-                
-                if scaleFromX > scaleMax {
-                    scaleFromX = scaleMax
-                }
-                
-                if scaleFromX < scaleMin {
-                    scaleFromX = scaleMin
-                }
-                
-                let scale = CATransform3DScale(transform, scaleFromX, scaleFromX, 1)
-                
-                cell.layer.transform = CATransform3DConcat(rotation, scale)
+                cell.layer.transform = animateCell(cellFrame: cellFrame)
             }
         }
+    }
+    
+    func animateCell(cellFrame: CGRect) -> CATransform3D {
+        let angleFromX = Double(-cellFrame.origin.x / 10)
+        let angle = CGFloat((angleFromX * Double.pi) / 180)
+        var transform = CATransform3DIdentity
+        transform.m34 = -1.0 / 1000
+        let rotation = CATransform3DRotate(transform, angle, 0, 1, 0)
+        
+        var scaleFromX = (1000 - (cellFrame.origin.x - 200)) / 1000
+        let scaleMax: CGFloat = 1.0
+        let scaleMin: CGFloat = 0.6
+        
+        if scaleFromX > scaleMax {
+            scaleFromX = scaleMax
+        }
+        
+        if scaleFromX < scaleMin {
+            scaleFromX = scaleMin
+        }
+        
+        let scale = CATransform3DScale(transform, scaleFromX, scaleFromX, 1)
+        
+        return CATransform3DConcat(rotation, scale)
     }
 }
 
@@ -113,6 +117,8 @@ extension StartViewController: UICollectionViewDelegate, UICollectionViewDataSou
         cell.titleLabel.text = section["title"]
         cell.captionLabel.text = section["caption"]
         cell.coverImageView.image = UIImage(named: section["image"]!)
+        
+        cell.layer.transform = animateCell(cellFrame: cell.frame)
         
         return cell
     }
