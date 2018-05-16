45b7ecfbc758591503528eaeff41657448d6f292
omrobbie <putu.roby@yahoo.com>
2018-05-16 11:09:17 +0800

    bikin animasi header dengan swift

--- DesignCodeApp/StartViewController.swift
+++ DesignCodeApp/StartViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class StartViewController: UIViewController {
     
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var deviceImageView: UIImageView!
+    @IBOutlet weak var playVisualView: UIVisualEffectView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        titleLabel.alpha = 0
+        deviceImageView.alpha = 0
+        playVisualView.alpha = 0
+        
+        UIView.animate(withDuration: 1) {
+            self.titleLabel.alpha = 1
+            self.deviceImageView.alpha = 1
+            self.playVisualView.alpha = 1
+        }
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
 }
