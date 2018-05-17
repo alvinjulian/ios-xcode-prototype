1c0236dc8e31a49ccfa8ff228cd73c553b3e1643
omrobbie <putu.roby@yahoo.com>
2018-05-17 10:48:08 +0800

    setup testimonial cell dengan data

--- DesignCodeApp/TestimonialViewController.swift
+++ DesignCodeApp/TestimonialViewController.swift
@@ -15,23 +15,27 @@ class TestimonialViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        testimonialCollectionView.delegate = self
+        testimonialCollectionView.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+extension TestimonialViewController: UICollectionViewDelegate, UICollectionViewDataSource  {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return testimonials.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "testimonialCell", for: indexPath) as! TestimonialCollectionViewCell
+        let testimonial = testimonials[indexPath.row]
+        
+        cell.textLabel.text = testimonial["text"]
+        cell.nameLabel.text = testimonial["name"]
+        cell.jobLabel.text = testimonial["job"]
+        cell.avatarImageView.image = UIImage(named: testimonial["avatar"]!)
+        
+        return cell
     }
-    */
-
 }
