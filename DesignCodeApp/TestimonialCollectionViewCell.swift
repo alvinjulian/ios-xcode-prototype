ba67d0be3a8c2c9abd4af2ef55b669ddb73bac6f
omrobbie <putu.roby@yahoo.com>
2018-05-17 10:06:41 +0800

    koneksikan antara outlet ke controller cell, dan tambahkan dummy data untuk testimonial

---
+++ DesignCodeApp/TestimonialCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TestimonialCollectionViewCell.swift
+//  DesignCodeApp
+//
+//  Created by omrobbie on 17/05/18.
+//  Copyright © 2018 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class TestimonialCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var jobLabel: UILabel!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    
+}
---
+++ DesignCodeApp/TestimonialViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TestimonialViewController.swift
+//  DesignCodeApp
+//
+//  Created by omrobbie on 17/05/18.
+//  Copyright © 2018 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class TestimonialViewController: UIViewController {
+
+    @IBOutlet weak var testimonialCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
