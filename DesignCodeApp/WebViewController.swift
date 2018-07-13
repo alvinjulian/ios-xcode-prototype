095a9b6563db62d7cd00923dff7f73fa7f65ead5
omrobbie <putu.roby@yahoo.com>
2018-07-13 17:58:50 +0700

    tambahkan info loading di title web view

--- DesignCodeApp/WebViewController.swift
+++ DesignCodeApp/WebViewController.swift
@@ -22,6 +22,21 @@ class WebViewController: UIViewController {
         let request = URLRequest(url: url)
         
         webView.load(request)
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            if webView.estimatedProgress == 1.0 {
+                navigationItem.title = webView.title
+            } else {
+                navigationItem.title = "Loading..."
+            }
+        }
+    }
+    
+    deinit {
+        webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress))
     }
     
     @IBAction func doneButtonTapped(_ sender: Any) {
